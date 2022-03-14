8c77357080afce7b5d9eeeed6f8620d35ce1c7dc
cif95 <vivianacifarelli9@gmail.com>
2022-03-14 15:30:51 +0100

    implemented milestone 1 and 2

--- main.js
+++ main.js
@@ -53,7 +53,106 @@ const posts = [
 		},
 		"likes": 95,
 		"created": "2021-03-05"
-	}
+	},
+	{
+		"id": 6,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=533",
+		"author": {
+			"name": "Alessia Franco",
+			"image": "https://unsplash.it/300/300?image=28"
+		},
+		"likes": 210,
+		"created": "2021-03-05"
+	},
+	{
+		"id": 7,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=530",
+		"author": {
+			"name": "Veralba Taldone",
+			"image": "https://unsplash.it/300/300?image=25"
+		},
+		"likes": 595,
+		"created": "2021-04-05"
+	},
+	{
+		"id": 8,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=524",
+		"author": {
+			"name": "Silvia Vergari",
+			"image": "https://unsplash.it/300/300?image=30"
+		},
+		"likes": 133,
+		"created": "2021-04-05"
+	},
+	{
+		"id": 9,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=550",
+		"author": {
+			"name": "Pasquale Orsini",
+			"image": "https://unsplash.it/300/300?image=19"
+		},
+		"likes": 5,
+		"created": "2021-06-05"
+	},
+	{
+		"id": 10,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=434",
+		"author": {
+			"name": "Valeria Rossi",
+			"image": "https://unsplash.it/300/300?image=39"
+		},
+		"likes": 280,
+		"created": "2021-07-05"
+	},
+	{
+		"id": 11,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=634",
+		"author": {
+			"name": "Agnese Fratelli",
+			"image": "https://unsplash.it/300/300?image=38"
+		},
+		"likes": 142,
+		"created": "2021-08-05"
+	},
+	{
+		"id": 12,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=250",
+		"author": {
+			"name": "Noemi Fortunato",
+			"image": "https://unsplash.it/300/300?image=12"
+		},
+		"likes": 156,
+		"created": "2021-08-05"
+	},
+	{
+		"id": 13,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=598",
+		"author": {
+			"name": "Giuseppe Buongiorno",
+			"image": "https://unsplash.it/300/300?image=15"
+		},
+		"likes": 124,
+		"created": "2021-08-05"
+	},
+	{
+		"id": 14,
+		"content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+		"media": "https://unsplash.it/600/400?image=634",
+		"author": {
+			"name": "Alberto Lorusso",
+			"image": "https://unsplash.it/300/300?image=23"
+		},
+		"likes": 415,
+		"created": "2021-09-05"
+	},
 ];
 
 
@@ -68,3 +167,40 @@ const posts = [
 // 1 - Formattare le date in formato italiano (gg/mm/aaaa)
 // 2 - Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
 // 3 - Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+
+posts.forEach((element)=> {
+	document.getElementById('container').innerHTML += `
+	<div class="post">
+	<div class="post__header">
+			<div class="post-meta">                    
+				<div class="post-meta__icon">
+					<img class="profile-pic" src=${element['author']['image']} alt=${element['author']['name']}>                    
+				</div>
+				<div class="post-meta__data">
+					<div class="post-meta__author">${element['author']['name']}</div>
+					<div class="post-meta__time">${element['created']}</div>
+				</div>                    
+			</div>
+	</div>
+	<div class="post__text">${element['content']}</div>
+	<div class="post__image">
+			<img src=${element['media']} alt="image post">
+	</div>
+	<div class="post__footer">
+			<div class="likes js-likes">
+				<div class="likes__cta">
+					<a class="like-button  js-like-button" href="#" data-postid="1">
+							<i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+							<span class="like-button__label">Mi Piace</span>
+					</a>
+				</div>
+				<div class="likes__counter">
+					Piace a <b id="like-counter-1" class="js-likes-counter">${element['likes']}</b> persone
+				</div>
+			</div> 
+	</div>            
+	</div>`;
+});
+
+
+
