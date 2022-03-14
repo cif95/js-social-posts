70a8d20411cbab26e9180d0767e67208212373df
cif95 <vivianacifarelli9@gmail.com>
2022-03-14 16:36:06 +0100

    implemented milestone 3

--- main.js
+++ main.js
@@ -171,36 +171,50 @@ const posts = [
 posts.forEach((element)=> {
 	document.getElementById('container').innerHTML += `
 	<div class="post">
-	<div class="post__header">
-			<div class="post-meta">                    
-				<div class="post-meta__icon">
-					<img class="profile-pic" src=${element['author']['image']} alt=${element['author']['name']}>                    
+		<div class="post__header">
+				<div class="post-meta">                    
+					<div class="post-meta__icon">
+						<img class="profile-pic" src=${element['author']['image']} alt=${element['author']['name']}>                    
+					</div>
+					<div class="post-meta__data">
+						<div class="post-meta__author">${element['author']['name']}</div>
+						<div class="post-meta__time">${element['created']}</div>
+					</div>                    
 				</div>
-				<div class="post-meta__data">
-					<div class="post-meta__author">${element['author']['name']}</div>
-					<div class="post-meta__time">${element['created']}</div>
-				</div>                    
-			</div>
-	</div>
-	<div class="post__text">${element['content']}</div>
-	<div class="post__image">
-			<img src=${element['media']} alt="image post">
-	</div>
-	<div class="post__footer">
-			<div class="likes js-likes">
-				<div class="likes__cta">
-					<a class="like-button  js-like-button" href="#" data-postid="1">
-							<i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-							<span class="like-button__label">Mi Piace</span>
-					</a>
-				</div>
-				<div class="likes__counter">
-					Piace a <b id="like-counter-1" class="js-likes-counter">${element['likes']}</b> persone
-				</div>
-			</div> 
-	</div>            
+		</div>
+		<div class="post__text">${element['content']}</div>
+		<div class="post__image">
+				<img src=${element['media']} alt="image post">
+		</div>
+		<div class="post__footer">
+				<div class="likes js-likes">
+					<div class="likes__cta">
+						<a class="like-button  js-like-button" href="#" data-postid="${element['id']}">
+								<i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+								<span class="like-button__label">Mi Piace</span>
+						</a>
+					</div>
+					<div class="likes__counter">
+						Piace a <b id="like-counter-${element['id']}" class="js-likes-counter">${element['likes']}</b> persone
+					</div>
+				</div> 
+		</div>            
 	</div>`;
 });
 
 
 
+// Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+const IdPostsLiked = [];
+
+document.querySelectorAll('a.js-like-button').forEach((element)=> {
+	element.addEventListener('click', function() {
+		this.classList.toggle('like-button--liked');
+		IdPostsLiked.push(parseInt(this.dataset.postid));
+		console.log(IdPostsLiked);
+		let likes = parseInt(this.parentNode.parentNode.children[1].children[0].innerHTML);
+		likes++;
+		this.parentNode.parentNode.children[1].children[0].innerHTML = likes;
+	});
+});
